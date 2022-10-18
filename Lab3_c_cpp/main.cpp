c1d04a5ddf8d69cc2e56fc28398831bb03c1b45c
HE_k_TO <eduardpur18@gmail.com>
2022-10-18 12:56:08 +0300

    rewrite lab3

--- Lab3_c_cpp/main.cpp
+++ Lab3_c_cpp/main.cpp
@@ -1,65 +1,91 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include <iostream>
+//#include <iomanip>
 
-int* patienceSort(int* arr, int size) {
-	int decks[size][size], i, j, min, pickedRow;
-
-	int* count = (int*)calloc(sizeof(int), size), * sortedArr = (int*)malloc(size * sizeof(int));
-
-	for (i = 0; i < size; i++) {
-		for (j = 0; j < size; j++) {
-			if (count[j] == 0 || (count[j] > 0 && decks[j][count[j] - 1] >= arr[i])) {
-				decks[j][count[j]] = arr[i];
-				count[j]++;
-				break;
-			}
-		}
-	}
-
-	min = decks[0][count[0] - 1];
-	pickedRow = 0;
-
-	for (i = 0; i < size; i++) {
-		for (j = 0; j < size; j++) {
-			if (count[j] > 0 && decks[j][count[j] - 1] < min) {
-				min = decks[j][count[j] - 1];
-				pickedRow = j;
-			}
-		}
-		sortedArr[i] = min;
-		count[pickedRow]--;
-
-		for (j = 0; j < size; j++)
-			if (count[j] > 0) {
-				min = decks[j][count[j] - 1];
-				pickedRow = j;
-				break;
-			}
-	}
-
-	free(count);
-	free(decks);
-
-	return sortedArr;
-}
-
-int main(int argC, char* argV[])
+int main()
 {
-	int* arr, * sortedArr, i;
+	int sequence[] = { 12, 3, 8, 10, 7, 5, 12, 8, 10 };
+	const int seq_len = sizeof(sequence) / sizeof(int);
 
-	if (argC == 0)
-		printf("Usage : %s <integers to be sorted separated by space>");
-	else {
-		arr = (int*)malloc((argC - 1) * sizeof(int));
+	int* sorted_sequence = new int[seq_len];
 
-		for (i = 1; i <= argC; i++)
-			arr[i - 1] = atoi(argV[i]);
+	int* sorting_plane_heads = new int[seq_len];
+	int** sorting_plane = new int* [seq_len];
+	int sorting_heaps_count = 0;
 
-		sortedArr = patienceSort(arr, argC - 1);
-
-		for (i = 0; i < argC - 1; i++)
-			printf("%d ", sortedArr[i]);
+	for (int i = 0; i < seq_len; ++i) 
+	{
+		sorting_plane[i] = new int[seq_len];
+		sorting_plane_heads[i] = 0;
 	}
 
+	// split the sequence into heaps
+	for (int i = 0; i < seq_len; ++i) 
+	{
+		for (int h = 0; h < seq_len; ++h)
+		{
+			int cur_head = sorting_plane_heads[h];
+
+			if (cur_head == 0 || sorting_plane[h][cur_head - 1] >= sequence[i])
+			{
+				if (cur_head == 0) 
+				{
+					++sorting_heaps_count;
+				}
+				sorting_plane[h][cur_head] = sequence[i];
+				++sorting_plane_heads[h];
+				break;
+			}
+		}
+	}
+
+	// for debugging
+
+	/*for (int y = 0; y < seq_len; ++y) 
+	{
+		std::cout << std::setw(9) << sorting_plane_heads[y];
+		for (int x = 0; x < seq_len; ++x)
+		{
+			std::cout << std::setw(9) << sorting_plane[y][x];
+		}
+		std::cout << std::endl;
+	}*/
+	
+	// min element we can skip
+	sorted_sequence[0] = sorting_plane[0][sorting_plane_heads[0] - 1];
+	--sorting_plane_heads[0];
+
+	// merge heaps
+	for (int i = 1; i < seq_len; ++i) 
+	{
+		int min = INT32_MAX;
+		int minH = 0;
+
+		// select min top element among all heaps
+		for (int h = 0; h < sorting_heaps_count; ++h) 
+		{
+			if (sorting_plane_heads[h] > 0 && sorting_plane[h][sorting_plane_heads[h] - 1] < min)
+			{
+				min = sorting_plane[h][sorting_plane_heads[h] - 1];
+				minH = h;
+			}
+		}
+
+		sorted_sequence[i] = min;
+		--sorting_plane_heads[minH];
+	}
+
+	// out
+	for (int i = 0; i < seq_len; ++i) 
+	{
+		std::cout << sorted_sequence[i] << std::endl;
+	}
+
+	for (int i = 0; i < seq_len; ++i) 
+	{
+		delete[] sorting_plane[i];
+	}
+	delete[] sorted_sequence;
+	delete[] sorting_plane;
+	delete[] sorting_plane_heads;
 	return 0;
 }
